653d510535a4b631559f5e2f06280852e01836c6
Matthew Sharpe <mattsharpe72@gmail.com>
2018-02-06 18:16:35 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,24 +1,23 @@
-// var cardOne = "queen";
-// var cardTwo = "queen";
-// var cardThree = "king";
-// var cardFour = "king";
-
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-var cardTwo = cards[3];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
+var checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	console.log("You found a match!");
+	}  else {
+	console.log("Sorry, try again.");
+	};
+}
 
-if (cardsInPlay.length === 2) {
-	if (cardOne === cardTwo) {
-		alert("You found a match!")
-	} else {
-		alert("Sorry, try again.");
-	}
-};
+var flipCard = function(cardId) {
+  cardsInPlay.push(cards[cardId]);
+  console.log("User flipped " + cards[cardId])
+
+  checkForMatch();
+  }; 
+
+
+flipCard(0);
+flipCard(2);
 
 
